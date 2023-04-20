fd06d3b89be8e71b0dadd362be6e1c1c48bcdcd2
Alexandr <ssumatoha@gmail.com>
2023-04-20 16:50:10 +0300

    modified:   controllers/noteController.js 	modified:   routs/noteRouter.js

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -1,10 +1,16 @@
-class NoteController {
-    async post(req, res) {
+const {Note} = require('../models/models')
+const ApiError = require('../error/apiError')
 
+class NoteController {
+    async create(req, res) {
+        const {text} = req.body
+        const note = await Note.create({text})
+        return res.json(note)
     }
 
     async get(req, res) {
-        
+        const notes = await Note.findAll()
+        return res.json(notes)
     }
 }
 
--- routs/noteRouter.js
+++ routs/noteRouter.js
@@ -2,7 +2,7 @@ const Router = require('express')
 const noteController = require('../controllers/noteController')
 const router = new Router()
 
-    router.post('/', noteController.post)
+    router.post('/', noteController.create)
     router.get('/', noteController.get)
 
 module.exports = router
