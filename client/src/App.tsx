11e24b72aa91dfbcfb814ebd34c40e5311521b58
Alexandr <ssumatoha@gmail.com>
2023-04-26 19:18:56 +0300

    modified:   client/src/App.tsx 	modified:   client/package-lock.json 	modified:   client/package.json 	modified:   client/src/components/Note.tsx 	client/src/components/Header.tsx 	client/src/components/Notes.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import { Note } from './components/Note';
+import React, { useEffect, useState } from 'react';
 import { notes } from './data/notes';
+import { Notes } from './components/Notes';
+import { Header } from './components/Header';
+import axios from 'axios';
+import { INotes } from './models';
 
 function App() {
+
+  // const [notes, setNotes] = useState()
+
+  async function fetchNotes() {
+    const res = await axios.get<INotes[]>('http://localhost:5000/api/note/')
+    console.log(res)
+  }
+
+  useEffect(() => {
+    fetchNotes()
+    }, [])
+
   return (
-    <div className='container mx-auto max-w-2xl pt-5'>
-        <Note note={notes[0].rows[0]}/>
+    <div className='bg-slate-700'>
+      <Header/>
+      <div className='container mx-auto max-w-2xl pt-5'>
+        <Notes notes={notes[0]}/>
+      </div>
     </div>
   );
 }
